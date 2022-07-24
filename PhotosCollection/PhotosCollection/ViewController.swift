e658dbb522246929b6e185a0d1d0c4fab78a4f0d
Kirill Klebanov <headmast@bk.ru>
2022-07-24 12:55:11 +0300

    Add two post request

--- PhotosCollection/PhotosCollection/ViewController.swift
+++ PhotosCollection/PhotosCollection/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Post {
+    let id: Int
+    let title: String
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        loadData {[weak self] post in
+            self?.resultLabel.text = "Success" + " " + String(post.id) + " " + post.title
+        }
     }
-
-
+    
+    private func loadData() {
+        let session = URLSession(configuration: .default)
+        guard let url = URL(string: "https://my-json-server.typicode.com/Headmast/lightJSONServer/posts/2") else {return}
+        let dataTask = session.dataTask(with: url) {data, response, error in
+            if let json = try? JSONSerialization.jsonObject(with: data!) as? [String:Any],
+               let id = json["id"] as? Int,
+               let title = json["title"] as? String {
+                //print ("Success" + " " + String(id) + " " + title)
+                DispatchQueue.main.async { [weak self]  in
+                    self?.resultLabel.text = "Success" + " " + String(id) + " " + title
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private func loadData(with completion: @escaping (_ post: Post) -> Void) {
+        let session = URLSession(configuration: .default)
+        guard let url = URL(string: "https://my-json-server.typicode.com/Headmast/lightJSONServer/posts/2") else {return}
+        let dataTask = session.dataTask(with: url) {data, response, error in
+            if let json = try? JSONSerialization.jsonObject(with: data!) as? [String:Any],
+               let id = json["id"] as? Int,
+               let title = json["title"] as? String {
+                let post = Post(id: id, title: title)
+                //print ("Success" + " " + String(id) + " " + title)
+                DispatchQueue.main.async {
+                    completion(post)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
 }
 
