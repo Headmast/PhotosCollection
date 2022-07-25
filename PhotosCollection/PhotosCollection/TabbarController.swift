4f68d4b8e38cf08becf7e408ba049d78415e6634
Kirill Klebanov <headmast@bk.ru>
2022-07-25 23:32:12 +0300

    Tabbar with two same controllers

---
+++ PhotosCollection/PhotosCollection/TabbarController.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  PhotosCollection
+//
+//  Created by Kirill Klebanov on 25.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class TabbarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .red
+        let firstVC = ViewController(title: "First")
+        let firstItem = UITabBarItem(title: "Async", image: UIImage(systemName: "drop"), selectedImage: UIImage(systemName: "drop.fill"))
+        firstVC.tabBarItem = firstItem
+        let secondVC = ViewController(title: "Second")
+        let secondItem = UITabBarItem(title: "Dispet", image: UIImage(systemName: "leaf"), selectedImage: UIImage(systemName: "leaf.fill"))
+        secondVC.tabBarItem = secondItem
+        self.viewControllers = [firstVC, secondVC]
+    }
+    
+}
