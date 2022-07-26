1c54e3af80873ca5d385d9fae5394b033e17ddfa
Kirill Klebanov <headmast@bk.ru>
2022-07-26 16:28:29 +0300

    Add Operation ViewConroller

---
+++ PhotosCollection/PhotosCollection/OperationsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  OperationsViewController.swift
+//  PhotosCollection
+//
+//  Created by Kirill Klebanov on 26.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class OperationsViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Operations"
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+        let constraints = [
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16)
+        ]
+        NSLayoutConstraint.activate(constraints)
+        
+        dura
+    }
+}
